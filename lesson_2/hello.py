aab2cfabcc80b40fd5aadd72161ce6374c805781
Karina Moskalevich <karinamoskalevich@MacBook-Air-Karina.local>
2018-03-28 22:28:10 +0300

    dz is ready

---
+++ lesson_2/hello.py
@@ -0,0 +1,10 @@
+_list = ['karina','katarina','viktor','sveta','anna','evgenya']
+print (_list[1])
+print (_list[1:4])
+print (_list[4:])
+_list.append ('vitalya')   
+_dict = {'moskalevich':'karina','maksimenko':'katarina','sveshnikov':'viktor',
+'volkova':'sveta','dmitrieva':'anna','sokolova':'evgenya'}
+print (_dict['maksimenko'])
+last_name = 'sveshnikov'
+print (_dict[last_name])
---
+++ lesson_2/meat.py
@@ -0,0 +1,21 @@
+day_of_week = 3 # день недели
+time = 15 #время
+if day_of_week == 1:
+    print ("monday")
+if day_of_week == 2:
+    print ("tuesday")
+if day_of_week == 3:
+    print ("wednesday")
+    if time < 12:
+    	print("a.m.")
+    else:
+   		print("p.m.")
+if day_of_week == 4:
+    print ("thursday")
+if day_of_week == 5:
+    print ("friday")
+if day_of_week == 6:
+    print ("saturday")
+if day_of_week == 7:
+    print ("sunday")
+    
